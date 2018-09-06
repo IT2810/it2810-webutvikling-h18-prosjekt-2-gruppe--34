486c7d197963b9b110c9a39e5fe01c8ce2ba479b
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-09-06 15:47:25 +0200

    hello-world app

--- pro2/src/index.js
+++ pro2/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const helloWorld = "Hello world !";
+
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+  function App() {
+    return (
+      <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+      </div>
+    );
+  }
+  
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
