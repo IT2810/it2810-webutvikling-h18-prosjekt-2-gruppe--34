59a67b4693ef44b0334a16e530439a25e1030652
Erikstelarsen <Erikstelarsen@outlook.com>
2018-09-13 10:28:03 +0200

    json filer laster nå ved ajax calls #7

---
+++ pro2/src/Components/radiobtnstekst.js
@@ -0,0 +1,25 @@
+const radiobtnstekst = [
+    {
+        name: 'Tekst',
+        key: 'einstein',
+        label: 'Einstein',
+        value: 'einstein',
+
+    },
+    {
+        name: 'Tekst',
+        key: 'gandhi',
+        label: 'Gandhi',
+        value: 'gandhi'
+
+    },
+    {
+        name: 'Tekst',
+        key: 'scott',
+        label: 'Scott',
+        value: 'michaelscott',
+
+    }
+];
+
+export default radiobtnstekst;
--- pro2/src/index.js
+++ pro2/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './style.css';
+import Header from './Components/Header.js';
+import Content from './Components/Content.js';
 import registerServiceWorker from './registerServiceWorker';
+
+class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+            <Header />
+            <Content />
+            </React.Fragment>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
