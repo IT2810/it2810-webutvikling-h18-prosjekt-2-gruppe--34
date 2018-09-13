a9596740740d3cf07a93aee3b13de5b9b9ddd507
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-09-13 13:00:50 +0200

    laget tab komponenter #10

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import './index.css';
-import RadiobtnContainer from './Components/RadiobtnContainer.js';
-
+import React, { Component } from "react";
+import "./App.css";
+import "./index.css";
+import RadiobtnContainer from "./Components/RadiobtnContainer.js";
+import TabContainer from "./Components/TabContainer.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="header">
-            <h1>UTSTILLINGSTITTEL</h1>
+          <h1>UTSTILLINGSTITTEL</h1>
         </div>
-        <div id="button" className="tab1"></div>
-        <div id="button" className="tab2"></div>
-        <div id="button" className="tab3"></div>
-        <div id="button" className="tab4"></div>
+        <TabContainer />
         <div className="content">
           <p>CONTENT</p>
           <div className="picture_box">
-          <img src="./favicon.ico" alt="Space Doggo"/>
+            <img src="./favicon.ico" alt="Space Doggo" />
           </div>
           <div className="quote_box">
             <p>MICHAEL SCOTT</p>
@@ -26,8 +23,7 @@ class App extends Component {
         </div>
         <div className="check_box">
           <p>CHECKBOX</p>
-            <RadiobtnContainer/>
-
+          <RadiobtnContainer />
         </div>
         <div className="footer">
           <h2>FOOTER</h2>
