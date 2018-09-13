fffe91e296bd41db9d6dd86ea3c4e56cb2e04994
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-09-13 09:39:54 +0200

    deleted start files from start-app command

--- pro2/src/index.js
+++ pro2/src/index.js
@@ -1,26 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-const helloWorld = "Hello world !";
-
-
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-  }
-  
-  function App() {
-    return (
-      <div>
-        <Welcome name="Sara" />
-        <Welcome name="Cahal" />
-        <Welcome name="Edite" />
-      </div>
-    );
-  }
-  
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
