ef069eee8d76b4e3bf4f102c6c83fd56a52dd143
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-09-18 18:28:35 +0200

    mobil styling startet. Laget et grid system

--- pro2/src/Components/group.jsx
+++ pro2/src/Components/group.jsx
@@ -3,19 +3,19 @@ import React, { Component } from "react";
 class Group extends Component {
   render() {
     return (
-      <div className="labels">
+      <div className={"group" + this.props.name}>
         {this.props.btns.map(btn => (
-          <label>{btn.id}
+          <label>
+            {btn.id}
             <input
-            name={btn.name}
-            type="radio"
-            key={btn.id}
-            id={btn.id}
-           /* onChange={btn.onPicChangeCategory} */
-            onClick={this.props.onMediaClick}
-          />
+              name={btn.name}
+              type="radio"
+              key={btn.id}
+              id={btn.id}
+              /* onChange={btn.onPicChangeCategory} */
+              onClick={this.props.onMediaClick}
+            />
           </label>
-
         ))}
       </div>
     );
