59b649364cefc1678d33adf0c240bbbe202f52a1
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-09-20 12:17:46 +0200

    tab valgt ved oppstart

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ registerServiceWorker();
 const animal = document.getElementById("animal");
 const einstein = document.getElementById("einstein");
 const random1 = document.getElementById("random1");
+const tab1 = document.getElementById("1");
+
 animal.setAttribute("checked", "checked");
 einstein.setAttribute("checked", "checked");
 random1.setAttribute("checked", "checked");
+tab1.setAttribute("class", "active");
