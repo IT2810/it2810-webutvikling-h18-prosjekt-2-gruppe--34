b610db072b55b74ec8e54c2c13c0e4b94492ba14
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-09-20 09:54:51 +0200

    lagt til checked radio buttons fra oppstart. Mangler aa laste inn media ved oppstart

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+const animal = document.getElementById("animal");
+const einstein = document.getElementById("einstein");
+const random1 = document.getElementById("random1");
+animal.setAttribute("checked", "checked");
+einstein.setAttribute("checked", "checked");
+random1.setAttribute("checked", "checked");
